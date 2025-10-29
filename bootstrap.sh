0eb1a42b2570a186c66487705d32947fe1eab08e
Artur Aukhatov <aaukhatov@gmail.com>
2025-10-29 20:23:35 +0100

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo xcode-select --install
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+brew cleanup
