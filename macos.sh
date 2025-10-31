95172747f37fe272b20c4169c614654848b676c4
Artur Aukhatov <aaukhatov@gmail.com>
2025-10-31 19:06:38 +0100

    disable tmutil

--- macos.sh
+++ macos.sh
@@ -88,8 +88,8 @@ defaults write com.apple.screencapture disable-shadow -bool true
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
-# Disable local Time Machine backups
-hash tmutil &> /dev/null && sudo tmutil disablelocal
+# Disable Time Machine backups
+hash tmutil &> /dev/null && sudo tmutil disable
 
 # Enable the debug menu in Disk Utility
 defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
