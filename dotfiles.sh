6fd20cf4b2db691b1a55f36265a8a88f3503740f
Artur Aukhatov <aaukhatov@gmail.com>
2025-11-08 00:24:48 +0100

    dotfiles activation script

--- dotfiles.sh
+++ dotfiles.sh
@@ -27,3 +27,28 @@ dotfiles() {
   info "  	${GREEN}${BOLD}cd ${clone_dir}/dotfiles${RESET}"
   info "  	${GREEN}${BOLD}stow -v -t \"\$HOME\" zsh git vim config${RESET}"
 }
+
+activate_dotfiles() {
+	local dotfiles_dir="${1:-}"
+  cd "$dotfiles_dir" || {
+    err "Cannot cd into $dotfiles_dir"
+    exit 1
+  }
+
+  # Collect only directories (packages)
+  local packages=()
+  mapfile -t packages < <(find . -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
+
+  if [[ ${#packages[@]} -eq 0 ]]; then
+    warn "No packages found in $dotfiles_dir."
+    return 0
+  fi
+
+  info "Activating dotfiles from $dotfiles_dir → $HOME"
+  for pkg in "${packages[@]}"; do
+    echo "   ↳ stowing package: $pkg"
+    stow -v -t "$HOME" "$pkg"
+  done
+
+  ok "Dotfiles activated successfully."
+}
