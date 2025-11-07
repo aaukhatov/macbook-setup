67c08ee2b6c5804b91815e0dccd24fe39aad5e91
Artur Aukhatov <aaukhatov@gmail.com>
2025-11-07 20:48:32 +0100

    refactoring .dotfiles installation

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,16 +1,26 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${SCRIPT_DIR}/utils.sh"
-
 dotfiles() {
 	local target_dotfiles_dir
-	repo_url="https://github.com/aaukhatov/macbook-setup.git"
+	local repo_url="https://github.com/aaukhatov/macbook-setup.git"
+  local repo_name
+
+  if ! command -v git >/dev/null 2>&1; then
+		warn "git is not installed"
+		return 1
+	fi
+
   repo_name=$(basename -s .git "$repo_url")
+
 	read -r -p "Provide an absolute path where you want to clone: " target_dotfiles_dir
+
 	if [[ ! -d "$target_dotfiles_dir" ]]; then
     mkdir -p "$target_dotfiles_dir"
   fi
-	run git clone "$repo_url" "${target_dotfiles_dir}/${repo_name}"
-	info "Run stow manually to apply .dotfiles"
+
+  clone_dir="${target_dotfiles_dir}/${repo_name}"
+
+	run git clone "$repo_url" "${clone_dir}"
+
+	info "Run 'stow' manually to apply .dotfiles from $clone_dir"
 }
