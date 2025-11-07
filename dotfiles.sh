0eac25bd2922dfb16a67a13979692fcc43c17bdb
Artur Aukhatov <aaukhatov@gmail.com>
2025-11-08 00:10:51 +0100

    more clear prompt message

--- dotfiles.sh
+++ dotfiles.sh
@@ -12,7 +12,7 @@ dotfiles() {
   repo_name=$(basename -s .git "$repo_url")
 
 	local target_dotfiles_dir
-	read -r -p "Provide an absolute path where you want to clone: " target_dotfiles_dir
+	read -r -p "Enter the path to clone the dotfiles repository (existing directory will be replaced): " target_dotfiles_dir
 
 	if [[ ! -d "$target_dotfiles_dir" ]]; then
     mkdir -p "$target_dotfiles_dir"
